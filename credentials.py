28051c4924ff8753f090e74b772179b93793b5b9
Marcus Rose <marcusr2626@gmail.com>
2019-03-23 23:42:00 -0600

    Initial commit, simple message sending

---
+++ credentials.py
@@ -0,0 +1,5 @@
+account_sid = "AC5f256acf6c8dd070268cbe1960377773"
+auth_token = "50a2642c08c25479904dbb7c21e589ec"
+my_cell = "+###########"
+# insert your phone number for the variable my_cell
+my_twilio = "+17274706394"
